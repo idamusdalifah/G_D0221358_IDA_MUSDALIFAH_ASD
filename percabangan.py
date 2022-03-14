87501afeaba24e75d3a45cc6da0629b2a3f2b467
wiwi22222 <wiwilestiani915@gmail.com>
2022-03-14 11:01:20 +0700

    komentar

---
+++ percabangan.py
@@ -0,0 +1,18 @@
+#percabangan if
+data = 70
+if data <80:
+    print("nilai A")
+
+#percabangan if else
+nilai = 95
+if nilai >85:
+    print ("selamat anda lulus")
+
+ #percabangan elif
+umur = 19
+if umur <20 :
+     print ("anda tidak dapat bekerja")   
+elif umur >15:
+    print ("anda belum dapat bekrja")
+else:
+     print("umur yang diinginkan tidak dapat diterima")        
---
+++ variabel_type_data.py
@@ -0,0 +1,10 @@
+a=14
+nama="ida musdalifah"
+umur= 19
+tinggi= 1.58
+menikah= False
+
+print(nama,type(nama))
+print(umur,type(umur))
+print(tinggi,type(tinggi))
+print(menikah,type(menikah))
